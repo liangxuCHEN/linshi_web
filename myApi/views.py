1e064d382b6dc4728e0a3ed66f5ab6d0b97b23bd
e <chenliangxu68@gmail.com>
2017-04-26 22:11:42 +0800

    updata function use rate

--- myApi/views.py
+++ myApi/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render, redirect,render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.http import StreamingHttpResponse
 from django.views.decorators.csrf import csrf_exempt
-# from myApi.single_use_rate import main_process
+from myApi.single_use_rate import main_process
 from django_api import settings
 from myApi.models import Userate
 
@@ -23,25 +23,24 @@ def single_use_rate(request):
         # 命名规则：x_y_width_height_border.png
         filename = '%s_%s_%s_%s_%s' %\
                    (data['shape_x'], data['shape_y'], data['width'], data['height'], data['border'])
-        use_rate = Userate.objects.get(name=filename)
+        use_rate = Userate.objects.filter(name=filename)
         if use_rate:
             content = {
-                'rate': use_rate.rate,
+                'rate': use_rate[0].rate,
                 'file_name': 'static/%s.png' % filename,
             }
             return HttpResponse(json.dumps(content), content_type="application/json")
-
-        path = os.path.join(settings.BASE_DIR, 'static')
-        path = os.path.join(path, filename)
-        # rate = main_process(data, path)
-        rate = 91.87
-        content = {
-            'rate': rate,
-            'file_name': 'static/%s.png' % filename,
-        }
-        new_use_rate = Userate(name=filename, rate=rate)
-        new_use_rate.save()
-        return HttpResponse(json.dumps(content), content_type="application/json")
+        else:
+            path = os.path.join(settings.BASE_DIR, 'static')
+            path = os.path.join(path, filename)
+            rate = main_process(data, path)
+            content = {
+                'rate': rate,
+                'file_name': 'static/%s.png' % filename,
+            }
+            new_use_rate = Userate(name=filename, rate=rate)
+            new_use_rate.save()
+            return HttpResponse(json.dumps(content), content_type="application/json")
     else:
         return render(request, 'use_rate.html')
 
